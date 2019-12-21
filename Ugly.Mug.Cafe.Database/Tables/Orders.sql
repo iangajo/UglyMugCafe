aa656e5967032ebf9d1193ce753e107d9ad74dcc
Christian Gajo <christian_gajo@yahoo.com>
2019-12-21 21:11:33 +0800

    Added Db Project

---
+++ Ugly.Mug.Cafe.Database/Tables/Orders.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Orders](
+	[OrderId] [int] IDENTITY(1,1) NOT NULL,
+	[Customer] [nvarchar](50) NOT NULL,
+	[OrderNumber] [uniqueidentifier] NOT NULL,
+	[Request] [nvarchar](max) NOT NULL,
+	[OrderDate] [datetime] NOT NULL,
+	[Status] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED 
+(
+	[OrderId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
---
+++ Ugly.Mug.Cafe.Database/Tables/Products.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Products](
+	[ProductId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Description] [nvarchar](3000) NULL,
+ CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
+(
+	[ProductId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
